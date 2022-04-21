14530dfd5b475f36defba18c330d605034515dcb
Fareez Borhanudin <fare4z@gmail.com>
2022-04-22 00:04:21 +0800

    's'

--- check_register.php
+++ check_register.php
@@ -11,14 +11,20 @@ $password = $_POST['password'];
 $vpassword = $_POST['vpassword'];
 
 
+// Email Validation
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo("Please include an '@' in the email address. '$email' is missing an '@'");
+} else { 
+
 echo "name : $name <br>
 nric : $nric <br>
+email : $email <br>
 gender : ".get_jantina($nric)." <br>
 dob : ".get_dob($nric)." <br>
 state : $state <br>
 password : $password <br>
 vpassword : $vpassword";
-
+}
 
 include_once 'include/footer.php'; 
 // echo var_dump($_POST);
